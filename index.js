5def4332d0756a3103eb4f95e9b5498adae89a26
Gantabya Kumar Bayda <gantabyakprottoy96@gmail.com>
2025-11-13 02:23:10 +0600

    Added env variable

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://eximflow-db:qzEDAu2YAMduSPgW@cluster0.7smyhy0.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7smyhy0.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
     serverApi: {
